34574c3355bb2cb90281e35a972b1d8b0315b798
lorena.machado <lorena.machado@cra.adv.br>
2022-12-28 15:58:44 -0300

    criando variaveis e imprimindo na tela

---
+++ aula8.py
@@ -0,0 +1,21 @@
+nome = 'Lorena'
+
+sobrenome = 'Mattos'
+
+idade = 29
+
+ano_nascimento = 1993
+
+maior_de_idade = idade >= 18
+
+altura_metros = 1.65
+
+
+print('Nome: ', nome)
+print('\nSobrenome: ', sobrenome)
+print('\nIdade: ', idade)
+
+print('\nAno de nascimento: ', ano_nascimento)
+print('\nÉ maior de idade? ', maior_de_idade)
+
+print('\nAltura em metros: ', altura_metros)
