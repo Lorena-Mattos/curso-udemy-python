8e555e3b64e961c9e9ab634c7feabab1a28b4a4e
lorena.machado <lorena.machado@cra.adv.br>
2022-12-28 14:53:09 -0300

    operadores lógicos e tipo de dado bool

---
+++ aula5.py
@@ -0,0 +1,13 @@
+# Tipo de dado bool (boolean)
+# Ao quetionar algo em um programa, só existem duas respostas possíveis:
+# sim (True) ou não (False)
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que questiona se um valor é igual a outro
+
+print(10 == 10) # True
+print(10 == 3) # False
+print(10 != 8) # Diferente de (True)
+
+print(10 <= 5) # Menor ou igual a (False)
+print(10 >= 5) # Maior ou igual a (True)
+
