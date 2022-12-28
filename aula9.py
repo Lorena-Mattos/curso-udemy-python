72becb41f557cf5c5e61dbe6455ca061cf41dfa9
lorena.machado <lorena.machado@cra.adv.br>
2022-12-28 16:26:27 -0300

    operadores aritmeticos

---
+++ aula9.py
@@ -0,0 +1,22 @@
+# Operadores aritméticos (matemática)
+
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # todo número após o ponto, ele não irá colocar as casas decimais
+print('Divisão inteira', divisao_inteira)
+
+exponeciacao = 2 ** 10
+print('Exponeciação', exponeciacao)
+
+modulo = 55 % 5 # resto da divisão
+print('Módulo', modulo)
