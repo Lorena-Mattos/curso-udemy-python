5b78e4d1b80fc53a150fbde7dd4136adb0e56adc
lorena.machado <lorena.machado@cra.adv.br>
2022-12-28 14:27:39 -0300

    tipos de aspas

---
+++ aula3.py
@@ -0,0 +1,19 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro das aspas
+"""
+
+# Aspas simples
+print('Lorena')
+
+# Aspas duplas
+print("Lorena")
+
+# Escape
+print("Lorena \"Mattos\"")
+
+# r
+print(r"Lorena \"Mattos\"")
