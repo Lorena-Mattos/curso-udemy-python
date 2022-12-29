1c3abc29958beb521ec39a98404b195e96f2b322
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 12:04:20 -0300

    acrescentando mais exemplo

--- logica-de-programação/aula25.py
+++ logica-de-programação/aula25.py
@@ -7,7 +7,12 @@ f - float
 x e x - Hexadecimal (ABCDEF0123456789)
 """
 
+# Exemplos:
+
 nome = 'Luiz'
 preco = 1000.95897643
 variavel = '%s, o preço total é R$%.2f' % (nome, preco)
 print(variavel)
+
+print(33 * '-')
+print('O hexadecimal de %d é %08X' % (1500, 1500)) # Pode ser usado 4 ou 8, com x maiúsculo para colocar letras maiúsculas ou x minusculo
