558230f387f23a95836c682622f5e21eaecc13c5
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 09:03:27 -0300

    corrigindo operadores aritméticos

--- logica-de-programação/aula12.py
+++ logica-de-programação/aula12.py
@@ -15,15 +15,18 @@ imc = int_peso / float_altura ** 2
 result = f'{nome} tem {float_altura:.2f} de altura, sua idade é {idade} anos e pesa {int_peso} Kg. Seu IMC é: {imc}'
 print(nome, result)
 
-if imc <= 18.5:
+if imc < 18.5:
     print(nome, 'cuidado, seu IMC indica magreza')
     
 elif imc >= 18.5:
     print(nome, 'seu IMC está normal')
+
 elif imc >= 24.9:
     print(nome, 'seu IMC indica sobrepeso')
-elif imc >= 30:
+
+elif imc > 30:
     print(nome, 'cuidado, seu IMC indica obesidade')
+
 else:
     print('Não há indicações de IMC')
 
