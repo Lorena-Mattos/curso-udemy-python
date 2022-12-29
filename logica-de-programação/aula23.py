433b1de374ebaf9ad870b35fcd02c592198d07bd
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 11:26:38 -0300

    operador logico: not

---
+++ logica-de-programação/aula23.py
@@ -0,0 +1,15 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+senha = input('Senha: ')
+
+if not senha:
+    print('Você não digitou nada')
+
+# O not é para inverter a expressão
+
+# print(not True) # False
+# print(not False) # True
+
