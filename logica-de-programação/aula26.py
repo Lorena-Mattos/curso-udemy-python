419860af30566e232287a0bbdc4871a144b2bd8b
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 12:38:36 -0300

    Formatação de strings com f-strings

---
+++ logica-de-programação/aula26.py
@@ -0,0 +1,31 @@
+"""
+Formatação de strings com f-strings
+
+s - string
+d - int
+f - float
+
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+
+> - Esquerda
+< - Direita
+^ - Centro
+
+Sinal: + ou -
+Ex.: 0>100,.1f
+Conversion flags - !r !s !a
+"""
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{variavel:$^10}.')
+print(f'{1000.4873645565646:,.1f}')
+print(f'{1000.4873645565646:.2f}')
+print(f'{1000.4873648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
