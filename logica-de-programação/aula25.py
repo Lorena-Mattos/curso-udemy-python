945255c42af8e62160b661ae7bed43c42abaf315
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 11:59:16 -0300

    interpolação de strings com %

---
+++ logica-de-programação/aula25.py
@@ -0,0 +1,13 @@
+"""
+Interpolação básica de strings
+
+s - string
+d e i - int
+f - float
+x e x - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = '%s, o preço total é R$%.2f' % (nome, preco)
+print(variavel)
