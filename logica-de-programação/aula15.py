b2f18b9194c3f92ec0610b86f384253a57159313
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 08:43:14 -0300

    aprendendo a função input

--- logica-de-programação/aula15.py
+++ logica-de-programação/aula15.py
@@ -1,1 +1,13 @@
-# Como coletar dados com input
+# Como coletar dados com input (função)
+
+# nome = input('Qual é seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 =  input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+
+
