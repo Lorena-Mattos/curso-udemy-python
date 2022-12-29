b7921d299f0996642ec898770fa917f0533acbb0
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 08:20:08 -0300

    usando função e parâmetro nomeado

--- logica-de-programação/aula14.py
+++ logica-de-programação/aula14.py
@@ -1,7 +1,11 @@
-# aprendendo usar format()
+# aprendendo usar a função format()
 a = 'A'
 b = 'B'
 c = 1.1
 
-formato = 'a={} b={} c={}'.format(a, b, c)
+string = 'a={nome1} b={nome2} c={nome3:.2f}'
+formato = string.format(
+  nome1=a, nome2=b, nome3=c)
 print(formato)
+
+# nome3=c é chamado de parâmetro, tudo que vier depois de um parâmetro nomeado, PRECISA ser nomeado
