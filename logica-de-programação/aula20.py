e5b7bb05aa6f1aff6efade2ffe301421ba9dff05
lorena.machado <lorena.machado@cra.adv.br>
2022-12-29 10:35:27 -0300

    Excercício com if e comparação

---
+++ logica-de-programação/aula20.py
@@ -0,0 +1,17 @@
+# Excercício com if e comparação
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+# Solucação:
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual '
+        f'ao que {segundo_valor=}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior '
+        f'do que {primeiro_valor=}'
+    )
