53f764c5b2ac5f1d1b5ac4fb512af7403b9c3ed3
lorena.machado <lorena.machado@cra.adv.br>
2023-01-02 15:55:30 -0300

    separando exemplos com #

--- logica-de-programação/aula45.py
+++ logica-de-programação/aula45.py
@@ -4,17 +4,25 @@ Iterador -> quem sabe entregar um valor por vez
 next -> me entregue o próximo valor
 iter -> me entregue seu iterador
 """
-# for letra in texto
+# for letra in texto:
+
 texto = 'Luiz'  # iterável
-
-# iteratador = iter(texto)  # iterator
-
-# while True:
-#     try:
-#         letra = next(iteratador)
-#         print(letra)
-#     except StopIteration:
-#         break
-
+    ##############
+iteratador = iter(texto)  # iterator
+    ##############
+while True:
+    try:
+        letra = next(iteratador)
+        print(letra)
+    except StopIteration:
+        break
+    ############
 for letra in texto:
     print(letra)
+    ###########
+texto = iter('Luiz') # __iter__()
+
+print(texto.__next__())
+print(texto.__next__())
+print(texto.__next__())
+print(texto.__next__())
