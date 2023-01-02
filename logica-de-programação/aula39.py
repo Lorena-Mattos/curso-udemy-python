4e2c885fa0234e77d17268e4c482eab6a484da31
lorena.machado <lorena.machado@cra.adv.br>
2023-01-02 11:32:00 -0300

    resolução de exercício iteração de str com while

--- logica-de-programação/aula39.py
+++ logica-de-programação/aula39.py
@@ -2,12 +2,29 @@
 Iterando strings com while
 """
 #       012345678910
-nome = 'Luiz Otávio'  # Iteráveis
-#      1110987654321
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
-print(nome[3])
+# nome = 'Luiz Otávio'  # Iteráveis
+#       1110987654321
+# tamanho_nome = len(nome)
+# print(nome)
+# print(tamanho_nome)
+# print(nome[3])
 
-nova_string = ''
-nova_string += '*L*u*i*z* *O*t*á*v*i*o'
+# nova_string = ''
+# nova_string += '*L*u*i*z* *O*t*á*v*i*o'
+
+
+# resolução de exercício de iteração de str com while
+nome = 'Luiz Otávio'  # Iteráveis
+
+tamanho_nome = len(nome)
+
+indice = 0
+
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
