3374c1ac9d8fcdf03d949ec0cc1877cab61b1be3
lorena.machado <lorena.machado@cra.adv.br>
2023-01-02 15:20:00 -0300

    adic comentário explicando os indices dentro de ()

--- logica-de-programação/aula44.py
+++ logica-de-programação/aula44.py
@@ -2,7 +2,7 @@
 For + Range
 range -> range(start, stop, step)
 """
-numeros = range(0, 100, 8)
+numeros = range(0, 100, 8) # (inicia em 0, vai até 10 e conta de 8 em 8)
 
 for numero in numeros:
     print(numero)
