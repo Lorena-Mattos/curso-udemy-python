74ed959f22bcda689e8c3c27270d463ccf1df605
lorena.machado <lorena.machado@cra.adv.br>
2023-01-03 17:55:51 -0300

    acrescentando nota

--- logica-de-programação/aula51.py
+++ logica-de-programação/aula51.py
@@ -1,5 +1,5 @@
 """
-Introdução ao empacotamento e desempacotamento
+Introdução ao empacotamento e desempacotamento + tuples (tuplas)
 """
 _, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
 print(nome)
