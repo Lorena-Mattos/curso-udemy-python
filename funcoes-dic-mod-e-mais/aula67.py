aec25db95abbf4e33de4baa624d2384e174cc6b2
lorena.machado <lorena.machado@cra.adv.br>
2023-01-04 16:05:45 -0300

    Valores padrão para parâmetros

--- funcoes-dic-mod-e-mais/aula67.py
+++ funcoes-dic-mod-e-mais/aula67.py
@@ -1,16 +1,22 @@
 """
-Argumentos nomeados e não nomeados em funções Python
-Argumento nomeado tem nome com sinal de igual
-Argumento não nomeado recebe apenas o argumento (valor)
+Valores padrão para parâmetros
+Ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será
+usado.
+Refatorar: editar o seu código.
 """
 
 
-def soma(x, y, z):
-    # Definição
-    print(f'{x=} y={y} {z=}', '|', 'x + y + z = ', x + y + z)
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
 
 
-soma(1, 2, 3)
-soma(1, y=2, z=5)
-
-print(1, 2, 3, sep='-')
+soma(1, 2)
+soma(3, 5)
+soma(100, 200)
+soma(7, 9, 0)
+soma(y=9, z=0, x=7)
