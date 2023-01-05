347c5d15eaba8dbafdb7e5516339edc7066a1d49
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 17:05:20 -0300

    importlib

---
+++ funcoes-dic-mod-e-mais/aula98_m.py
@@ -0,0 +1,15 @@
+import importlib
+
+import aula98_m
+
+print(aula98_m.variavel)
+
+for i in range(10):
+    importlib.reload(aula98_m)
+    print(i)
+
+print('Fim')
+
+print(123)
+
+variavel = 'Luiz 1'
