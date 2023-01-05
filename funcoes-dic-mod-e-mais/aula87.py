659dc51f177146bd4bd147107bd6f88d7b51ba62
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 14:26:08 -0300

    isinstace p/ saber se objeto é de determinado tipo

---
+++ funcoes-dic-mod-e-mais/aula87.py
@@ -0,0 +1,27 @@
+# isinstace - para saber se objeto é de determinado tipo
+lista = [
+    'a', 1, 1.1, True, [0, 1, 2], (1, 2),
+    {0, 1}, {'nome': 'Luiz'},
+]
+
+for item in lista:
+    if isinstance(item, set):
+        print('SET')
+        item.add(5)
+        print(item, isinstance(item, set))
+
+    elif isinstance(item, str):
+        print('STR')
+        print(item.upper())
+
+    elif isinstance(item, (int, float)):
+        print('NUM')
+        print(item, item * 2)
+    else:
+        print('OUTRO')
+        print(item)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
