e652e30f340bf5a402be229b1027f4f5cc4455c3
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 09:34:22 -0300

    shallow copy vs deep copy em dados mutáveis

--- funcoes-dic-mod-e-mais/aula76.py
+++ funcoes-dic-mod-e-mais/aula76.py
@@ -70,3 +70,20 @@ print(pessoa['idade'])
 
 # for chave, valor in pessoa.items():
 #     print(chave, valor)
+
+# Outro exemplo, shallow copy
+
+import copy
+
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2],
+}
+d2 = d1.copy()
+
+d2['c1'] = 1000
+d2['l1'][1] = 999999
+
+print(d1)
+print(d2)
