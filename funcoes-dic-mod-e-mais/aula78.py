1493eca66b0bc554c661905793aebcab5c380037
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 10:23:11 -0300

    Métodos úteis: add, update, clear, discard

--- funcoes-dic-mod-e-mais/aula78.py
+++ funcoes-dic-mod-e-mais/aula78.py
@@ -22,6 +22,16 @@ s1 = {'Luiz', 1, 2, 3}  # com dados
 # Métodos úteis:
 # add, update, clear, discard
 
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá mundo')
+s1.discard('Luiz')
+print(s1)
+
+
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
