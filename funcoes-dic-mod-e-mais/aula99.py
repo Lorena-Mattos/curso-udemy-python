7ed86c465b03870c7ae72602b2ccce4c08a6f0a7
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 17:45:36 -0300

    corrigindo

--- funcoes-dic-mod-e-mais/aula99.py
+++ funcoes-dic-mod-e-mais/aula99.py
@@ -1,8 +1,5 @@
 # from sys import path
 
-import aula99_package.modulo
-from aula99_package import modulo
-from aula99_package.modulo import *
 # import aula99_package.modulo
 # from aula99_package import modulo
 # from aula99_package.modulo import *
@@ -10,12 +7,6 @@ from aula99_package.modulo import *
 # from aula99_package.modulo import soma_do_modulo
 # # from aula99_package.modulo import soma_do_modulo
 
-# print(*path, sep='\n')
-print(soma_do_modulo(1, 2))
-print(aula99_package.modulo.soma_do_modulo(1, 2))
-print(modulo.soma_do_modulo(1, 2))
-print(variavel)
-print(nova_variavel)
 # # print(*path, sep='\n')
 # print(soma_do_modulo(1, 2))
 # print(aula99_package.modulo.soma_do_modulo(1, 2))
--- funcoes-dic-mod-e-mais/aula99_package/modulo.py
+++ funcoes-dic-mod-e-mais/aula99_package/modulo.py
@@ -7,8 +7,8 @@ from aula99_package.modulo_b import fala_oi
 
 variavel = 'Alguma coisa'
 
-# def soma_do_modulo(x, y):
-#     return x + y
+def soma_do_modulo(x, y):
+    return x + y
 
 
 nova_variavel = 'OK'
