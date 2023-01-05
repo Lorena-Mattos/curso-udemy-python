80613a9d2a36eae25856ebb969abdf2a6f7df661
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 17:50:56 -0300

    acrescentando exemplos

--- funcoes-dic-mod-e-mais/aula99.py
+++ funcoes-dic-mod-e-mais/aula99.py
@@ -13,7 +13,13 @@
 # print(modulo.soma_do_modulo(1, 2))
 # print(variavel)
 # print(nova_variavel)
-from aula99_package.modulo import fala_oi, soma_do_modulo
 
-print(__name__)
-fala_oi()
+# from aula99_package.modulo import fala_oi, soma_do_modulo
+
+# print(__name__)
+# fala_oi()
+
+from aula99_package import falar_oi, soma_do_modulo
+
+print(soma_do_modulo(2, 3))
+falar_oi()
