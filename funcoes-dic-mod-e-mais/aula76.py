5247dac6796dd71446979bfa8a019ea9a1f588e7
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 09:42:27 -0300

    Outro exemplo de get

--- funcoes-dic-mod-e-mais/aula76.py
+++ funcoes-dic-mod-e-mais/aula76.py
@@ -87,3 +87,28 @@ d2['l1'][1] = 999999
 
 print(d1)
 print(d2)
+
+# Outro exemplo de get
+
+p1 = {
+    'nome': 'Luiz',
+    'sobrenome': 'Miranda',
+}
+# print(p1['nome'])
+# print(p1.get('nome', 'Não existe'))
+
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
+# p1.update({
+#     'nome': 'novo valor',
+#     'idade': 30,
+# })
+# p1.update(nome='novo valor', idade=30)
+# tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
