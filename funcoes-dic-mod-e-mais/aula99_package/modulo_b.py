d053f8201f7e5bcfd5f23d2cf3785ef6f426d914
lorena.machado <lorena.machado@cra.adv.br>
2023-01-05 17:43:17 -0300

    correção

--- funcoes-dic-mod-e-mais/aula99_package/modulo_b.py
+++ funcoes-dic-mod-e-mais/aula99_package/modulo_b.py
@@ -1,13 +1,2 @@
-#     'soma_do_modulo',
-#     'nova_variavel',
-# ]
-from aula99_package.modulo_b import fala_oi
-
-variavel = 'Alguma coisa'
-
-def soma_do_modulo(x, y):
-    return x + y
-
-
-nova_variavel = 'OK'
-# fala_oi()
+def fala_oi():
+    print('oi')
