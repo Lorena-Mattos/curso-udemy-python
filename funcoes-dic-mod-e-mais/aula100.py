4ca0ee07b03c9a12ed701c8bfff5e77f428e487d
lorena.machado <lorena.machado@cra.adv.br>
2023-01-06 08:13:56 -0300

    correções

--- funcoes-dic-mod-e-mais/aula100.py
+++ funcoes-dic-mod-e-mais/aula100.py
@@ -6,7 +6,6 @@ from dados import produtos
 # Exercícios
 # Aumente os preços dos produtos a seguir em 10%
 # Gere novos_produtos por deep copy (cópia profunda)
-
 novos_produtos = [
     {**p, 'preco': round(p['preco'] * 1.1, 2)}
     for p in copy.deepcopy(produtos)
@@ -16,6 +15,8 @@ novos_produtos = [
 # print()
 # print(*novos_produtos, sep='\n')
 
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 produtos_ordenados_por_nome = sorted(
     copy.deepcopy(produtos),
     key=lambda p: p['nome'],
@@ -25,8 +26,9 @@ produtos_ordenados_por_nome = sorted(
 # print()
 # print(*produtos_ordenados_por_nome, sep='\n')
 
-# Ordene os produtos por nome decrescente (do maior para menor)
-# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 produtos_ordenados_por_preco = sorted(
     copy.deepcopy(produtos),
     key=lambda p: p['preco']
@@ -41,6 +43,3 @@ print()
 print(*produtos_ordenados_por_nome, sep='\n')
 print()
 print(*produtos_ordenados_por_preco, sep='\n')
-
-# Ordene os produtos por preco crescente (do menor para maior)
-# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
