6e7291cacb51fa76084e369d3e510f79f3740f27
lorena.machado <lorena.machado@cra.adv.br>
2023-01-09 11:46:08 -0300

    Usamos a função open para abrir um arquivo em Python (ele pode ou não existir)

--- funcoes-dic-mod-e-mais/aula116.py
+++ funcoes-dic-mod-e-mais/aula116.py
@@ -6,35 +6,35 @@
 caminho_arquivo = 'aula116.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
-# #
+# 
 # arquivo.close()
 # with open(caminho_arquivo, 'w') as arquivo:
 #     print('Olá mundo')
 #     print('Arquivo vai ser fechado')
 
-with open(caminho_arquivo, 'w+') as arquivo:
-    arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n')
-    arquivo.writelines(
-        ('Linha 3\n', 'Linha 4\n')
-    )
-    arquivo.seek(0, 0)
-    print(arquivo.read())
-    print('Lendo')
-    arquivo.seek(0, 0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
 
-    print('READLINES')
-    arquivo.seek(0, 0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
 
-print('#' * 10)
+# print('#' * 10)
 
-with open(caminho_arquivo, 'r') as arquivo:
-    print(arquivo.read())
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
 
 # r (leitura), w (escrita), x (para criação)
 # a (escreve ao final), b (binário)
@@ -53,4 +53,43 @@ with open(caminho_arquivo, 'r') as arquivo:
 # json.dump = Gera um arquivo json
 # json.load
 
+# Leia também: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
+# with open (context manager) e métodos úteis do TextIOWrapper
+# Usamos a função open para abrir
+# um arquivo em Python (ele pode ou não existir)
+# #
+# arquivo.close()
 
+with open(caminho_arquivo, 'w+') as arquivo:
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+	with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+		arquivo.write('Atenção\n')
+		arquivo.write('Linha 1\n')
+		arquivo.write('Linha 2\n')
+		arquivo.writelines(
+			('Linha 3\n', 'Linha 4\n')
+		)
