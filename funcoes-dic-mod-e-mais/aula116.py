b9c1eeaa4a1bd4faeddcd292329a8241aa0779d6
lorena.machado <lorena.machado@cra.adv.br>
2023-01-09 11:32:37 -0300

    With open, context e métodos úteis d TextIOWrapper

--- funcoes-dic-mod-e-mais/aula116.py
+++ funcoes-dic-mod-e-mais/aula116.py
@@ -1,7 +1,41 @@
 # Criando arquivos com Python + Context Manager with
+# With open (context manager) e métodos úteis do TextIOWrapper
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
 # Modos:
+caminho_arquivo = 'aula116.txt'
+
+# arquivo = open(caminho_arquivo, 'w')
+# #
+# arquivo.close()
+# with open(caminho_arquivo, 'w') as arquivo:
+#     print('Olá mundo')
+#     print('Arquivo vai ser fechado')
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    print('Lendo')
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+print('#' * 10)
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
+
 # r (leitura), w (escrita), x (para criação)
 # a (escreve ao final), b (binário)
 # t (modo texto), + (leitura e escrita)
@@ -18,11 +52,5 @@
 # Vamos falar mais sobre o módulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
-caminho_arquivo = 'aula116.txt'
 
-# arquivo = open(caminho_arquivo, 'w')
-# #
-# arquivo.close()
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá mundo')
-    print('Arquivo vai ser fechado')
+
