183a4b77301f14faac3dc54203dce05b161e38c1
lorena.machado <lorena.machado@cra.adv.br>
2023-01-09 16:36:39 -0300

    solução de exercício

--- into-poo-classes/aula127.py
+++
@@ -1,5 +0,0 @@
-# Exercício - Salve sua classe em JSON
-# Salve os dados da sua classe em JSON
-# e depois crie novamente as instâncias
-# da classe com os dados salvos
-# Faça em arquivos separados.
