0ca67b2db7aad2c29a4b549174954d1b53a0cbdb
lorena.machado <lorena.machado@cra.adv.br>
2023-01-09 09:52:39 -0300

    pip - instalando pacotes e bibliotecas

--- funcoes-dic-mod-e-mais/aula115.py
+++ funcoes-dic-mod-e-mais/aula115.py
@@ -14,3 +14,17 @@
 # Abra a pasta do seu projeto no terminal
 # e digite:
 # python -m venv venv
+
+# Linux e Mac: source venv/bin/activate
+# Desativar: deactivate
+#
+# pip - instalando pacotes e bibliotecas
+# Instalar última versão:
+# pip install nome_pacote
+# Instalar versão precisa
+# (tem outras formas também não mencionadas)
+# pip install nome_pacote==0.0.0
+# Desinstalar pacote
+# pip uninstall nome_pacote
+# Congelar (ver pacotes)
+# pip freeze
