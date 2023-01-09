46f69c8bdb8ba862834add13716db337ecb0b185
lorena.machado <lorena.machado@cra.adv.br>
2023-01-09 11:58:28 -0300

    os.remove, unlink, os.rename apag, renom e movendo

--- funcoes-dic-mod-e-mais/aula116.py
+++ funcoes-dic-mod-e-mais/aula116.py
@@ -1,41 +1,10 @@
-# Criando arquivos com Python + Context Manager with
-# With open (context manager) e métodos úteis do TextIOWrapper
+import os
+
+# Leia também: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
+# with open (context manager) e métodos úteis do TextIOWrapper
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
 # Modos:
-caminho_arquivo = 'aula116.txt'
-
-# arquivo = open(caminho_arquivo, 'w')
-# 
-# arquivo.close()
-# with open(caminho_arquivo, 'w') as arquivo:
-#     print('Olá mundo')
-#     print('Arquivo vai ser fechado')
-
-# with open(caminho_arquivo, 'w+') as arquivo:
-#     arquivo.write('Linha 1\n')
-#     arquivo.write('Linha 2\n')
-#     arquivo.writelines(
-#         ('Linha 3\n', 'Linha 4\n')
-#     )
-#     arquivo.seek(0, 0)
-#     print(arquivo.read())
-#     print('Lendo')
-#     arquivo.seek(0, 0)
-#     print(arquivo.readline(), end='')
-#     print(arquivo.readline().strip())
-#     print(arquivo.readline().strip())
-
-#     print('READLINES')
-#     arquivo.seek(0, 0)
-#     for linha in arquivo.readlines():
-#         print(linha.strip())
-
-# print('#' * 10)
-
-# with open(caminho_arquivo, 'r') as arquivo:
-#     print(arquivo.read())
-
 # r (leitura), w (escrita), x (para criação)
 # a (escreve ao final), b (binário)
 # t (modo texto), + (leitura e escrita)
@@ -52,15 +21,12 @@ caminho_arquivo = 'aula116.txt'
 # Vamos falar mais sobre o módulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
+caminho_arquivo = 'aula116.txt'
 
-# Leia também: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
-# with open (context manager) e métodos úteis do TextIOWrapper
-# Usamos a função open para abrir
-# um arquivo em Python (ele pode ou não existir)
+# arquivo = open(caminho_arquivo, 'w')
 # #
 # arquivo.close()
 
-with open(caminho_arquivo, 'w+') as arquivo:
 # with open(caminho_arquivo, 'w+') as arquivo:
 #     arquivo.write('Linha 1\n')
 #     arquivo.write('Linha 2\n')
@@ -86,10 +52,13 @@ with open(caminho_arquivo, 'w+') as arquivo:
 # with open(caminho_arquivo, 'r') as arquivo:
 #     print(arquivo.read())
 
-	with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
-		arquivo.write('Atenção\n')
-		arquivo.write('Linha 1\n')
-		arquivo.write('Linha 2\n')
-		arquivo.writelines(
-			('Linha 3\n', 'Linha 4\n')
-		)
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+    arquivo.write('Atenção\n')
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+
+# os.remove(caminho_arquivo) # ou unlink
+# os.rename(caminho_arquivo, 'aula116-2.txt')
