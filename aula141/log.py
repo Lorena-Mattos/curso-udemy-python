e3546491aa40bab98b1247cde8f01e1efaf8d213
lorena.machado <lorena.machado@cra.adv.br>
2023-01-10 11:37:23 -0300

    LogFileMixin e a união de tudo

--- aula141/log.py
+++ aula141/log.py
@@ -1,5 +1,9 @@
 # Abstração
 # Herança - é um
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 
 class Log:
     def _log(self, msg):
@@ -14,7 +18,12 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_fomatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log:', msg_fomatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_fomatada)
+            arquivo.write('\n')
+
 
 class LogPrintMixin(Log):
     def _log(self, msg):
@@ -22,6 +31,9 @@ class LogPrintMixin(Log):
 
 
 if __name__ == '__main__':
-    l = LogPrintMixin()
-    l.log_error('qualquer coisa')
-    l.log_success('Que legal')
+    lp = LogPrintMixin()
+    lp.log_error('qualquer coisa')
+    lp.log_success('Que legal')
+    lf = LogFileMixin()
+    lf.log_error('qualquer coisa')
+    lf.log_success('Que legal')
