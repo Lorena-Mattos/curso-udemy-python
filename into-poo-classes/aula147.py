59826676f26ceb069a1663653fc19861bee13cfb
lorena.machado <lorena.machado@cra.adv.br>
2023-01-11 07:45:36 -0300

    ex de uso de dunder methods

--- into-poo-classes/aula147.py
+++ into-poo-classes/aula147.py
@@ -17,22 +17,29 @@
 # __repr__(self) - str
 
 class Ponto:
-    def __init__(self, x, y, z='String'):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.z = z
-
-    def __str__(self):
-        return f'({self.x}, {self.y})'
 
     def __repr__(self):
-        # class_name = self.__class__.__name__
         class_name = type(self).__name__
-        return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
+        return f'{class_name}(x={self.x!r}, y={self.y!r})'
+
+    def __add__(self, other):
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Ponto(novo_x, novo_y)
+
+    def __gt__(self, other):
+        resultado_self = self.x + self.y
+        resultado_other = other.x + other.y
+        return resultado_self > resultado_other
 
 
-p1 = Ponto(1, 2)
-p2 = Ponto(978, 876)
-print(p1)
-print(repr(p2))
-print(f'{p2!r}')
+if __name__ == '__main__':
+    p1 = Ponto(4, 2)  # 6
+    p2 = Ponto(6, 4)  # 10
+    p3 = p1 + p2
+    print(p3)
+    print('P1 é maior que p2', p1 > p2)
+    print('P2 é maior que p1', p2 > p1)
