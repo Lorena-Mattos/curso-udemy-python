537f6d1aa7f055d64ad8d703327e4606c667f4fd
lorena.machado <lorena.machado@cra.adv.br>
2023-01-11 09:02:54 -0300

    acrescentando mais informações

--- into-poo-classes/aula149.py
+++ into-poo-classes/aula149.py
@@ -32,10 +32,19 @@ class MyOpen:
     def __exit__(self, class_exception, exception_, traceback_):
         print('FECHANDO ARQUIVO')
         self._arquivo.close()
+		
+		# raise class_exception(*exception_.args).with_traceback(traceback_)
+
+        # print(class_exception)
+        # print(exception_)
+        # print(traceback_)
+        # exception_.add_note('Minha nota')
+
+        # return True  # Tratei a exceção
 
 
 with MyOpen('aula149.txt', 'w') as arquivo:
     arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n')
+    arquivo.write('Linha 2\n', 123)
     arquivo.write('Linha 3\n')
     print('WITH', arquivo)
