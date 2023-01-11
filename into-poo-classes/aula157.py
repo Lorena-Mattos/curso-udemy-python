afc040ba6b34f057003064c37aa569f3bfbcc4ee
lorena.machado <lorena.machado@cra.adv.br>
2023-01-11 17:30:24 -0300

    Direco = enum.Enum(Direcoes, [ESQUERDA, DIREITA])

--- into-poo-classes/aula157.py
+++ into-poo-classes/aula157.py
@@ -15,11 +15,29 @@
 # chave = Classe.chave.name
 # valor = Classe.chave.value
 
-def mover(direcao):
-    print(f'Movendo para {direcao}')
+import enum
+
+# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
+
+class Direcoes(enum.Enum):
+    ESQUERDA = enum.auto()
+    DIREITA = enum.auto()
+    ACIMA = enum.auto()
+    ABAIXO = enum.auto()
 
 
-mover('esquerda')
-mover('direita')
-mover('acima')
-mover('abaixo')
+print(Direcoes(1), Direcoes['ESQUERDA'], Direcoes.ESQUERDA)
+print(Direcoes(1).name, Direcoes.ESQUERDA.value)
+
+
+def mover(direcao: Direcoes):
+    if not isinstance(direcao, Direcoes):
+        raise ValueError('Direção não encontrada')
+
+    print(f'Movendo para {direcao.name} ({direcao.value})')
+
+
+mover(Direcoes.ESQUERDA)
+mover(Direcoes.DIREITA)
+mover(Direcoes.ACIMA)
+mover(Direcoes.ABAIXO)
