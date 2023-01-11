187ffc1ba4040a9f16de7c585fd02d7289838e2a
lorena.machado <lorena.machado@cra.adv.br>
2023-01-11 16:33:55 -0300

    DocStrings em class (Documentação)

--- aula156/main.py
+++ aula156/main.py
@@ -1,8 +1,9 @@
 #import uma_linha
-import varias_linhas
+# import documentando_funcoes
+import documentando_class
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
-help(varias_linhas)
+help(documentando_class)
