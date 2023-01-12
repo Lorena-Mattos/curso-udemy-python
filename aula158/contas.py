078825851a8305f1674e14eb717784993a4517f5
lorena.machado <lorena.machado@cra.adv.br>
2023-01-12 08:05:48 -0300

    alterações de bloco de códigos

--- aula158/contas.py
+++ aula158/contas.py
@@ -2,27 +2,21 @@ import abc
 
 
 class Conta(abc.ABC):
-    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
+    def __init__(self, agencia, conta, saldo=0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
     @abc.abstractmethod
-    def sacar(self, valor: float) -> float: ...
+    def sacar(self, valor): ...
 
-    def depositar(self, valor: float) -> float:
+    def depositar(self, valor):
         self.saldo += valor
         self.detalhes(f'(DEPÓSITO {valor})')
-        return self.saldo
 
-    def detalhes(self, msg: str = '') -> None:
+    def detalhes(self, msg=''):
         print(f'O seu saldo é {self.saldo:.2f} {msg}')
         print('--')
-
-    def __repr__(self):
-        class_name = type(self).__name__
-        attrs = f'({self.agencia!r}, {self.conta!r}, {self.saldo!r})'
-        return f'{class_name}{attrs}'
 
 
 class ContaPoupanca(Conta):
@@ -36,18 +30,14 @@ class ContaPoupanca(Conta):
 
         print('Não foi possível sacar o valor desejado')
         self.detalhes(f'(SAQUE NEGADO {valor})')
-        return self.saldo
 
 
 class ContaCorrente(Conta):
-    def __init__(
-        self, agencia: int, conta: int,
-        saldo: float = 0, limite: float = 0
-    ):
+    def __init__(self, agencia, conta, saldo=0, limite=0):
         super().__init__(agencia, conta, saldo)
         self.limite = limite
 
-    def sacar(self, valor: float) -> float:
+    def sacar(self, valor):
         valor_pos_saque = self.saldo - valor
         limite_maximo = -self.limite
 
@@ -59,13 +49,6 @@ class ContaCorrente(Conta):
         print('Não foi possível sacar o valor desejado')
         print(f'Seu limite é {-self.limite:.2f}')
         self.detalhes(f'(SAQUE NEGADO {valor})')
-        return self.saldo
-
-    def __repr__(self):
-        class_name = type(self).__name__
-        attrs = f'({self.agencia!r}, {self.conta!r}, {self.saldo!r}, '\
-            f'{self.limite!r})'
-        return f'{class_name}{attrs}'
 
 
 if __name__ == '__main__':
