d2cf2c14f31213dabd015cb52d7dbe80c3f96fc8
lorena.machado <lorena.machado@cra.adv.br>
2023-01-12 11:27:31 -0300

    refatorando trecho de código

--- into-poo-classes/aula159.py
+++ into-poo-classes/aula159.py
@@ -5,16 +5,20 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Pessoa:
-    nome: str
-    idade: int
+    nome: str = field(
+        default='MISSING', repr=False
+    )
+    sobrenome: str = 'Not sent'
+    idade: int = 100
+    enderecos: list[str] = field(default_factory=list)
 
 
 if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 30)
-    p2 = Pessoa('Luiz', 30)
-    print(p1 == p2)
+    p1 = Pessoa()
+    # print(fields(p1))
+    print(p1)
