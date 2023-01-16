bc2bb569b6a7c33d8f3ffa7bbe15eee5a0e7dadb
lorena.machado <lorena.machado@cra.adv.br>
2023-01-16 08:19:29 -0300

    adicionando nota

--- modulos-python/aula184.py
+++ modulos-python/aula184.py
@@ -1,3 +1,41 @@
+
+"""
+python-dotenv é uma biblioteca Python que permite que você faça uso de arquivos
+de configuração para armazenar e acessar as suas variáveis de ambiente de forma
+mais fácil e segura em seus projetos.
+
+As variáveis de ambiente são valores que podem ser usados em seu código e que
+podem variar dependendo do ambiente em que o seu código está sendo executado
+(por exemplo, o ambiente de produção ou o ambiente de desenvolvimento).
+
+Para utilizar o python-dotenv, basta instalá-lo com o pip e, em seguida,
+adicionar um arquivo chamado .env na raiz do seu projeto.
+
+# Ative seu ambiente virtual
+pip install python-dotenv
+Esse arquivo deve conter as suas variáveis de ambiente e seguir o
+seguinte formato:
+
+# .env
+VARIAVEL_DE_AMBIENTE_1=valor
+VARIAVEL_DE_AMBIENTE_2=valor
+VARIAVEL_DE_AMBIENTE_3=valor
+Em seu código, você pode acessar essas variáveis usando o módulo os e
+a função os.getenv(), por exemplo:
+
+import os
+valor_da_variavel_1 = os.getenv("VARIAVEL_DE_AMBIENTE_1")
+O python-dotenv funciona lendo o arquivo .env e adicionando as variáveis de
+ambiente ao ambiente do sistema operacional,
+de forma que elas fiquem disponíveis
+para seu código usando a função os.getenv().
+
+Isso é útil, por exemplo, para não expor senhas ou outras
+informações confidenciais em seu código ou em repositórios de código
+compartilhados, pois o arquivo .env pode ser adicionado ao seu arquivo
+"""
+
+
 # Variáveis de ambiente com Python
 # Para variáveis de ambiente
 # Windows PS: $env:VARIAVEL="VALOR" | dir env:
