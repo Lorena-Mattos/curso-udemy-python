c1f5e50e7b6f50fe683e10d010d1d7ed8b156bf9
lorena.machado <lorena.machado@cra.adv.br>
2023-01-23 15:56:13 -0300

    arquivo sql

---
+++ bases-de-dados-sqlite-mysql-mariadb/agenda/Untitled-1.sql
@@ -0,0 +1,5 @@
+-- database: c:\Users\lorena.machado\PycharmProjects\curso-udemy-python\bases-de-dados-sqlite-mysql-mariadb\agenda\agenda.db
+
+-- Shift+Enter or Ctrl+Shift+Q (query): Executes a statement on the cursor.
+-- Ctrl+Shift+E (executescript): Executes the entire file without displaying the results.
+SELECT * FROM "agenda";
