1b68767851434525ec4e502acca2dc5369ff540c
Lorena <lorena.mattos6@gmail.com>
2023-10-03 11:05:22 -0300

    update

--- modulos-python/scraping.py
+++ modulos-python/scraping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://pt.stackoverflow.com/questions/tagged/python'
 response = requests.get(url)
-html = BeautifulSoup(response.text, 'html.parser')
+html = BeautifulSoup(response.text, 'html.parser', redirects=False)
 
 for pergunta in html.select('.question-summary'):
     titulo = pergunta.select_one('.question-hyperlink')
